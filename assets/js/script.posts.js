8c1f3a3fd5bfbcba75b68a49b8d6130423abdf62
Marcelo Lima <marcelo.lima4@aluno.senai.br>
2023-08-08 14:31:55 -0300

    feat: create store post Function

--- assets/js/script.posts.js
+++ assets/js/script.posts.js
@@ -1,3 +1,5 @@
+const posts = [];
+
 function savePost(){
     const title = document.getElementById("title").value;
     const category = document.getElementById("category").value;
@@ -5,9 +7,13 @@ function savePost(){
     const author = document.getElementById("author").value;
     const date = document.getElementById("date").value;
 
-    console.log(title, resume, author, date)
-    
-    cleanFields()
+    if(title && category && resume && author && date){
+        storePost(title, category, resume, author, date);
+        cleanFields();
+    }else{
+        alert("Preencha todos os campos!");
+    }
+
 }
 
 
@@ -17,4 +23,19 @@ function cleanFields(){
     document.getElementById("resume").value = "";
     document.getElementById("author").value = "";
     document.getElementById("date").value = "";
+
+    
+}
+function storePost(title, category, resume, author, date){
+    const post = {
+        title,
+        category,
+        resume,
+        author,
+        date
+
+    };
+    posts.push(post);
+
+    console.log(posts);
 }
