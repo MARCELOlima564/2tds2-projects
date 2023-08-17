6014c5a7333e15806dd1dc7336e26fa82ac30956
Marcelo Lima <marcelo.lima4@aluno.senai.br>
2023-08-17 10:19:08 -0300

    Feat: add class Product

--- assets/js/scripit.category.js
+++ assets/js/scripit.category.js
@@ -1,6 +1,18 @@
+//Categoria possui vários produtos 
 class Category {
     constructor(id, name) {
         this.id = id;
         this.name = name;
+        this.products = []
     }
 }
+
+class Product{
+    constructor(id, name, price, category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+
